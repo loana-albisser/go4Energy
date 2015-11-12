058d078dce2acf32f7cf53b8864a193f64d9e781
tcalbiss <loana.albisser@stud.hslu.ch>
2015-11-12 16:21:32 +0100

    Add Mulitplayer Screen

--- app/src/main/java/com/example/loana/go4energy/MainActivity.java
+++ app/src/main/java/com/example/loana/go4energy/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.loana.go4energy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void startMultiplayer(View view) {
+        Intent intent = new Intent(this, MultiplayerActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/loana/go4energy/MultiplayerActivity.java
@@ -0,0 +1,15 @@
+package com.example.loana.go4energy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Loana on 12.11.2015.
+ */
+public class MultiplayerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplayer);
+    }
+}
